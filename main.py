cfbbe53be8962e1c1ea100d585f4073619d7433f
Artur Z <arturziianbaev@gmail.com>
2023-10-20 16:13:41 -0700

    open the game (dinosaur game) without internet

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import pyautogui
+import time
+#check current position of the mouse
+currentMouseX, currentMouseY = pyautogui.position()
+print(currentMouseX,currentMouseY)
+
+pyautogui.click(1254,1059)
+time.sleep(2)
+pyautogui.write('Hzxc')
+time.sleep(1)
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.press('space')
