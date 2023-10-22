0ed5f0114c959cbae4fa012096c14113810e368e
Artur Z <arturziianbaev@gmail.com>
2023-10-22 09:45:37 -0700

    open google with the help of Google Icon picture

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import pyautogui
+#install open-cv to use confidence function
+#pip install opencv-python
+
 import time
 #check current position of the mouse
 currentMouseX, currentMouseY = pyautogui.position()
 print(currentMouseX,currentMouseY)
 
-#Your own X,Y Coordinates
-POSITION_OF_GOOGLEBROWSER_ICON_X=1254
-POSITION_OF_GOOGLEBROWSER_ICON_Y=1059
-#-------------------------------MAIN--------------------------MAIN---------------------------------MAIN--------------------------------#
 
-pyautogui.click(POSITION_OF_GOOGLEBROWSER_ICON_X,POSITION_OF_GOOGLEBROWSER_ICON_Y)
-time.sleep(2)
-pyautogui.write('Hzxc')
+#-------------------------------MAIN--------------------------MAIN---------------------------------MAIN--------------------------------#
+def old_version_to_find_google_icon():
+
+    #Your own X,Y Coordinates
+    POSITION_OF_GOOGLEBROWSER_ICON_X=1254
+    POSITION_OF_GOOGLEBROWSER_ICON_Y=1059
+    #OLD VERSION WITH COORDINATES
+    pyautogui.click(POSITION_OF_GOOGLEBROWSER_ICON_X,POSITION_OF_GOOGLEBROWSER_ICON_Y)
+    time.sleep(2)
+    pyautogui.write('Hzxc')
+    time.sleep(1)
+    pyautogui.press('enter')
+    time.sleep(1)
+    pyautogui.press('space')
+
+google=pyautogui.locateCenterOnScreen('google_icon.png',confidence=0.8)
+print(google.x)
+print(google.y)
+
 time.sleep(1)
-pyautogui.press('enter')
-time.sleep(1)
-pyautogui.press('space')
+
+pyautogui.click(google.x,google.y)
